abc5d2fbe1f5c31d09a86de926e6e277fe824f10
Hadeel <81963417+2172116630@users.noreply.github.com>
2021-06-22 10:01:10 +0000

    Requirement3

--- ecoomerce/views.py
+++ ecoomerce/views.py
@@ -12,3 +12,11 @@ def unavailable_product(request):
   poll_list = Product.objects.filter(status=False)
   data['unavailable'] = poll_list
   return render(request, "unavailable_product.html", context=data)
+
+def product_detail(request, product_id):
+  data={}
+  product_list = Product.objects.get(id = product_id)
+  comment_list = product_list.comment_set.all()
+  data['product'] = product_list
+  data['comment'] = comment_list
+  return render(request, "product_detail.html", context=data)
--- mysite/urls.py
+++ mysite/urls.py
@@ -1,11 +1,11 @@
-
+#from django.conf.urls import include, url
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path #, include
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('ecommerce/', include('ecommerce.urls')),    
+    #path('ecommerce/', include('ecommerce.urls')),    
    
 ]
 
