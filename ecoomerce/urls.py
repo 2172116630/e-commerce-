0528fd4a46041836e6d57736ab3fe95939c8aa9a
Naeimah Alsaleh <81956386+Naeimah@users.noreply.github.com>
2021-06-22 11:06:50 +0000

    urls.py

--- ecoomerce/urls.py
+++ ecoomerce/urls.py
@@ -2,14 +2,14 @@ from django.urls import path
 
 
 
-from .views import available_product, unavailable_poduct, product_detail
+from .views import available_product, unavailable_poduct, product_detail, product_comment
 
 app_name = 'ecommerce'
 urlpatterns = [
     path('available/', available_product),
     path('unavailable/', unavailable_poduct),
     path('show/product/<int:product_id>/', product_detail),
-    path('<slug:slug>/', views.product_comment, name='product_comment')
+    path('<slug:slug>/', product_comment, name='product_comment')
 
 ]
 
