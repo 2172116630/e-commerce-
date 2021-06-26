9e1f23a4fb64f8eb08090dc00232bab10e11758b
Hadeel <81963417+2172116630@users.noreply.github.com>
2021-06-26 14:41:21 +0000

    Req8&9

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import available_product, unavailable_product, product_detail, product_comment
+from .views import available_product, unavailable_product, product_detail, product_comment, add_to_cart, remove_from_cart 
 
 app_name = 'ecommerce'
 urlpatterns = [
@@ -7,6 +7,8 @@ urlpatterns = [
     path('unavailable/', unavailable_product),
     path('show/product/<int:product_id>/', product_detail),
     path('<slug:slug>/', product_comment, name='product_comment')
+    path('add-to-cart/<slug>/', add_to_cart, name='add_to_cart'),
+    path('remove-from-cart/<slug>/', remove_from_cart, name='remove-from-cart'),
 
 ]
 
